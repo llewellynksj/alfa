53c7a7b6b2d8694d6d8afd0b90b41419568049e4
Kate L <llewellyn.ksj@gmail.com>
2024-06-26 10:42:57 +0100

    Remove profile options from navbar

---
+++ views/services.view.php
@@ -0,0 +1,21 @@
+<?php require "partials/header.php"; ?>
+
+<?php require "partials/nav.php"; ?>
+
+<header class="py-10">
+  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <h1 class="text-3xl font-bold tracking-tight text-white">Services</h1>
+  </div>
+</header>
+</div>
+
+<main class="-mt-32">
+<div class="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
+  <div class="rounded-lg bg-white px-5 py-6 shadow sm:px-6">
+    <h2>ALFA</h2>
+    <p>Bricklaying Services</p>
+  </div>
+</div>
+</main>
+
+<?php require "partials/footer.php"; ?>
