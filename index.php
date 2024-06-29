acf42bbd637b23707cd0d5b31f69e6f4c1f9a67d
Kate L <llewellyn.ksj@gmail.com>
2024-06-29 10:31:52 +0100

    Refactor router.php

--- index.php
+++ index.php
@@ -2,18 +2,4 @@
 
 require 'functions.php';
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-  '/' => 'controllers/index.php',
-  '/services' => 'controllers/services.php',
-  '/about' => 'controllers/about.php',
-  '/contact' => 'controllers/contact.php',
-];
-
-if (array_key_exists($uri, $routes)) {
-  require $routes[$uri];
-} else {
-  http_response_code(404);
-  require 'views/404.php';
-}
+require 'router.php';
