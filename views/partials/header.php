d158d193ff40835e6a4cb687163975ee40434c1b
Kate L <llewellyn.ksj@gmail.com>
2024-07-03 10:17:06 +0100

    Amend styles on header and nav

--- views/partials/header.php
+++ views/partials/header.php
@@ -14,5 +14,5 @@
 <body class="h-full">
 
 <div class="min-h-full">
-  <div class="bg-gray-800 pb-32 pt-12">
+  <div class="bg-cyan-600 pb-32 pt-12">
     
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,6 +1,6 @@
-<nav class="bg-gray-800">
+<nav class="bg-cyan-600">
       <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div class="border-gray-700">
+        <div class="border-cyan-600">
 
           <div class="flex h-16 items-center justify-between px-4 sm:px-0">
 
@@ -15,11 +15,10 @@
                   <p class="text-white px-3"><em>ALFA Bricklaying Services</em></p>
                 </div>
                 <div class="ml-10 flex items-baseline space-x-4 hidden md:block">
-                  <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                  <a href="/" class="<?= uriIs('/') ?> rounded-md  px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-                  <a href="/about" class="<?= uriIs('/about') ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">About</a>
-                  <a href="/services" class="<?= uriIs('/services') ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Services</a>
-                  <a href="/contact" class="<?= uriIs('/contact') ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Contact</a>
+                  <a href="/" class="<?= uriIs('/') ?> rounded-md  px-3 py-2 text-md font-medium" aria-current="page">Home</a>
+                  <a href="/about" class="<?= uriIs('/about') ?> rounded-md px-3 py-2 text-md font-medium hover:bg-gray-700 hover:text-white">About</a>
+                  <a href="/services" class="<?= uriIs('/services') ?> rounded-md px-3 py-2 text-md font-medium hover:bg-gray-700 hover:text-white">Services</a>
+                  <a href="/contact" class="<?= uriIs('/contact') ?> rounded-md px-3 py-2 text-md font-medium hover:bg-gray-700 hover:text-white">Contact</a>
                 </div>
               </div>
               
